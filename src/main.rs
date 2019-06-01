21fb18c71591f2eb3b1a09b9849c88876aaf3a2c
Natnael Getahun <connect@ngetahun.me>
2019-06-01 23:14:16 +0200

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-use pcap::{Device};
+use pcap::{Device, Capture};
 
 #[macro_use]
 extern crate clap;
 use clap::{App};
+mod monitor;
 
 fn main() {
-    // let devices = Device::list().unwrap();
-    // for device in devices.iter() {
-    //     println!("Found a device {}", device.name);
-    // }
-
+    // Load YAML file
     let yml = load_yaml!("../cli.yml");
     let m = App::from_yaml(yml).get_matches();
 
 
     if m.is_present("list") {
-        println!("List flag present");
         let devices = Device::list().unwrap();
         let no_description = "No description found.";
         for device in devices.iter() {
@@ -24,15 +20,23 @@ fn main() {
         }
     }
 
-    // let main_device = Device::lookup().unwrap();
-    // println!("Main listening device {:?}", main_device.name);
-    // let mut cap = Capture::from_device(main_device).unwrap()
-    //                   .promisc(true)
-    //                   .snaplen(5000)
-    //                   .open().unwrap();
+    if m.is_present("default") {
+        let main_device = Device::lookup().unwrap();
+        println!("Main listening device {:?}", main_device.name);
+    }
 
-    // while let Ok(packet) = cap.next() {
-    //     println!("received packet! {:?}", packet);
-    // }
+    if m.is_present("stat") {    
+        let main_device = Device::lookup().unwrap();
+        println!("Main listening device {:?}", main_device.name);
+        let mut cap = Capture::from_device(main_device).unwrap()
+                        .promisc(true)
+                        .snaplen(5000)
+                        .open().unwrap();
+        // let bandwidth_monitor = monitor::BandwidthMonitor::new();
+        while let Ok(packet) = cap.next() {
+            println!("capture length: {}, packet length: {}", packet.header.caplen, packet.header.len);
+            // bandwidth_monitor.add_recieved_bytes(packet.header.caplen);
+            // println!("Bandwidth {:?}", bandwidth_monitor);
+        }
+    }
 }
-
